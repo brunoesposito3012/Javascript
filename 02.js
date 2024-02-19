a25862ca43a5ea76753be41ed077d37d6f24bed8
Bruno Campagnoli Soares da Costa Esposito <122655406+brunoesposito3012@users.noreply.github.com>
2024-02-19 16:58:47 -0300

    Add files via upload

---
+++ 02.js
@@ -0,0 +1,34 @@
+//diaSemana = prompt('Qual o dia da semana?');
+//console.log(diaSemana)
+//if (diaSemana == 'Sábado'){
+//    alert('Bom final de semana!');
+//}
+//else if (diaSemana =='Domingo'){
+//    alert('Bom final de semana!');
+//} else{
+//    alert('Boa semana!');
+//}
+
+//numero = prompt('Digite um numero');
+//if (numero > 0){
+//   alert('Numero positivo');
+//} else if(numero == 0){
+//    alert('Numero neutro');
+//} else{
+//   alert('Numero negativo');
+//}
+
+
+//pontuacao = 105;
+//if (pontuacao >= 100){
+//    console.log('Parabéns, você venceu!');
+//} else{
+//    console.log('Tente novamente para ganhar');
+//}
+
+
+saldoconta = 1500;
+alert(`Olá Bruno, o valor da sua conta é ${saldoconta}`)
+
+//let nome = prompt('Digite o seu nome')
+//alert(`Seja muito bem vindo ${nome}`);
