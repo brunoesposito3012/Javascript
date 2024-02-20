fec3473593d39004db34ec921fa70780c0380f9d
Bruno Campagnoli Soares da Costa Esposito <122655406+brunoesposito3012@users.noreply.github.com>
2024-02-20 14:30:38 -0300

    Add files via upload

---
+++ 04.js
@@ -0,0 +1,27 @@
+//let contador = 1;
+//while(contador <= 10){
+//    alert(contador);
+//    contador++;
+//}
+
+//let contador = 10;
+//while(contador >= 0){
+//    alert(contador);
+//    contador--;
+//}
+
+//let numero = prompt('Digite um numero para contagem regressiva');
+//while(numero > 0){
+//    numero --;
+//    console.log(numero)
+//}
+
+let contador = prompt ("Digite um número para Contagem Progressiva")
+let numero = 0; 
+while (contador >= numero) {
+  console.log(numero)
+  numero++;
+}
+
+
+
