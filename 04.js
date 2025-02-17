7911761e41c68533ffe100a188416c53a6eacc1d
Bruno Campagnoli Soares da Costa Esposito <122655406+brunoesposito3012@users.noreply.github.com>
2025-02-17 17:24:20 -0300

    Update 04.js

--- 04.js
+++ 04.js
@@ -1,20 +1,20 @@
-//let contador = 1;
-//while(contador <= 10){
-//    alert(contador);
-//    contador++;
-//}
+let contador = 1;
+while(contador <= 10){
+  alert(contador);
+  contador++;
+}
 
-//let contador = 10;
-//while(contador >= 0){
-//    alert(contador);
-//    contador--;
-//}
+let contador = 10;
+while(contador >= 0){
+    alert(contador);
+    contador--;
+}
 
-//let numero = prompt('Digite um numero para contagem regressiva');
-//while(numero > 0){
-//    numero --;
-//    console.log(numero)
-//}
+let numero = prompt('Digite um numero para contagem regressiva');
+while(numero > 0){
+    numero --;
+    console.log(numero)
+}
 
 let contador = prompt ("Digite um número para Contagem Progressiva")
 let numero = 0; 
@@ -22,6 +22,3 @@ while (contador >= numero) {
   console.log(numero)
   numero++;
 }
-
-
-
